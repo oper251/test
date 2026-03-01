8ebe82b43b7bd65075dc6d9d1b6b32b480de0042
oper251 <97793694+oper251@users.noreply.github.com>
2026-03-01 18:39:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'cache-' + new URL(location.href).searchParams.get('v');
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(['/', '/icon192.png']))
+));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+));
+
+
+
+//self.addEventListener('install', () => self.skipWaiting());
+//self.addEventListener('activate', (event) => {
+//    event.waitUntil(clients.claim());
+//});
+//self.addEventListener('fetch', (event) => {
+//    // Не кэшируем HTML
+//    if (event.request.mode === 'navigate') {
+//        event.respondWith(fetch(event.request));
+//        return;
+//    }
+//    // Остальное — как есть
+//    event.respondWith(fetch(event.request));
+//});
