45064b3f1857c721732aeef57c98521b8565ef5f
oper251 <97793694+oper251@users.noreply.github.com>
2026-03-02 17:56:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,69 @@
-const CACHE = "20260309--";
-const BASE = "/test/";
+const CACHE = "temp-cache";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll([BASE + "index.html", BASE + "manifest.json", BASE + "icon192.png"]);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE) {
-            console.log("Удалили CACHE: " + key);
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((key) => caches.delete(key)));
     }).then(() => {
-      // Отправляем сообщение ТОЛЬКО после полной активации и удаления старых кэшей
-      sendMessageToClients('Активирована новая версия: ' + CACHE);
+      self.clients.claim();
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-       sendMessageToClients('++: ' + CACHE);
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
 
-function sendMessageToClients(text) {
-  setTimeout(() => {
-    self.clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({text: text});
-      });
-    });
-  }, 1500);
-}
+
+
+//const CACHE = "20260309--";
+//const BASE = "/test/";
+//
+//self.addEventListener("install", (event) => {
+//  event.waitUntil(
+//    caches.open(CACHE).then((cache) => {
+//      return cache.addAll([BASE + "index.html", BASE + "manifest.json", BASE + "icon192.png"]);
+//    })
+//  );
+//});
+//
+//self.addEventListener("activate", (event) => {
+//  event.waitUntil(
+//    caches.keys().then((keys) => {
+//      return Promise.all(
+//        keys.map((key) => {
+//          if (key !== CACHE) {
+//            console.log("Удалили CACHE: " + key);
+//            return caches.delete(key);
+//          }
+//        })
+//      );
+//    }).then(() => {
+//      // Отправляем сообщение ТОЛЬКО после полной активации и удаления старых кэшей
+//      sendMessageToClients('Активирована новая версия: ' + CACHE);
+//    })
+//  );
+//});
+//
+//self.addEventListener("fetch", (event) => {
+//  event.respondWith(
+//    caches.match(event.request).then((response) => {
+//       sendMessageToClients('++: ' + CACHE);
+//      return response || fetch(event.request);
+//    })
+//  );
+//});
+//
+//function sendMessageToClients(text) {
+//  setTimeout(() => {
+//    self.clients.matchAll().then(clients => {
+//      clients.forEach(client => {
+//        client.postMessage({text: text});
+//      });
+//    });
+//  }, 1500);
+//}
